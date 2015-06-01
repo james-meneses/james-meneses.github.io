a1b86186ce14a2aa66099db4238f996f164611a9
James <jhemerson.comercial@gmail.com>
2015-06-01 01:35:28 -0300

    Sobre Concluído - Reorganizando Website

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,23 @@
 $( document ).ready(function() {
+  $(function() {
+    smoothScroll(500);
+  });
 
-  //
+  //smoothScroll
+  function smoothScroll(duration){
+    $('a[href^="#"]').on('click', function(event){
+
+      var target = $($(this).attr('href'));
+
+      if(target.length){
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, duration);
+      }
+
+    });
+  }
+
 
 });
